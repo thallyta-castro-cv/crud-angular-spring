c95df6dc5847d22bcc781e137a31f55088976f4c
thallyta-castro-cv <thallyta180136319@gmail.com>
2022-09-10 20:39:26 -0300

    feat: criado api em spring para listar, listar por id, criar e atualizar registro

---
+++ crud-spring/crud-spring/src/main/java/com/thallyta/crudspring/controllers/CourseController.java
@@ -0,0 +1,57 @@
+package com.thallyta.crudspring.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thallyta.crudspring.models.Course;
+import com.thallyta.crudspring.repositories.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+    
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+        public ResponseEntity<Course> getById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+        .map(course -> ResponseEntity.ok().body(course))
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+        return courseRepository.findById(id)
+        .map(courseFound -> {
+            course.setName(course.getName());
+            course.setCategory(course.getCategory());
+            Course updated = courseRepository.save(courseFound);
+            return ResponseEntity.ok().body(updated);
+        })
+        .orElse(ResponseEntity.notFound().build());
+    }
+}
---
+++ crud-spring/crud-spring/src/main/java/com/thallyta/crudspring/repositories/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.thallyta.crudspring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thallyta.crudspring.models.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long>{
+
+}
