d6cbff245d123236faeb655fa4a5f46e03e35cf2
thallyta-castro-cv <thallyta180136319@gmail.com>
2023-04-22 20:30:12 -0300

    feat: atualizado versão do spring para 3.0.0

--- crud-angular/src/app/features/course-form/course-form.component.ts
+++ crud-angular/src/app/features/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 
 import { ActivatedRoute } from '@angular/router';
 import { finalize, Subject, takeUntil } from 'rxjs';
@@ -15,10 +15,10 @@ import { FeaturesService } from './../services/features.service';
 })
 export class CourseFormComponent implements OnInit{
 
-  form = this.formBuilder.group({
+  public form = this.formBuilder.group({
     id: [''],
-    name: [''],
-    category: ['']
+    name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+    category: ['', Validators.required]
   });
 
   public loading: boolean;
@@ -51,6 +51,14 @@ export class CourseFormComponent implements OnInit{
       name: course.name,
       category: course.category
     });
+  }
+
+  save(): void {
+    if (this.courseId) {
+      this.update();
+    } else {
+      this.create();
+    }
   }
 
   create(): void {
@@ -87,16 +95,28 @@ export class CourseFormComponent implements OnInit{
     });
   }
 
-  save(): void {
-    if (this.courseId) {
-      this.update();
-    } else {
-      this.create();
-    }
-  }
-
   onCancel(): void {
     this.location.back();
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength: number = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 200;
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo Inválido';
   }
 
 }
