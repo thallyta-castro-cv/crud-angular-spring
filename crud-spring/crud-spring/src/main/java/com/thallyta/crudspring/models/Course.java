37a365fa91c7cfb3d1bde46277bc4a3f121525c3
thallyta-castro-cv <thallyta180136319@gmail.com>
2023-04-22 20:25:54 -0300

    feat: incluido soft delete (remoção lógica) no CRUD de cursos

--- crud-spring/crud-spring/src/main/java/com/thallyta/crudspring/models/Course.java
+++ crud-spring/crud-spring/src/main/java/com/thallyta/crudspring/models/Course.java
@@ -1,27 +1,43 @@
 package com.thallyta.crudspring.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
 @Table(name = "tb_courses")
+@SQLDelete(sql = "UPDATE tb_courses SET status = 'Inactive' WHERE id = ?")
+@Where(clause = "status = 'Active'")
 public class Course {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "name", length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    @Column(name = "name", length = 100, nullable = false)
     private String name;
 
-    @Column(name = "category", length = 30, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Back-end|Front-end")
+    @Column(name = "category", length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Active|Inactive")
+    @Column(name = "status", length = 10, nullable = false)
+    private String status = "Active";
 
 }
--- crud-spring/crud-spring/src/test/java/com/thallyta/crudspring/CrudSpringApplicationTests.java
+++ crud-spring/crud-spring/src/test/java/com/thallyta/crudspring/CrudSpringApplicationTests.java
@@ -1,13 +1,1 @@
-package com.thallyta.crudspring;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CrudSpringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
